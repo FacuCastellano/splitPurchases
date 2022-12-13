3c0186a4cb944ab518183499430fa4e250132963
Facundo Castellano <castellanofacundo@gmail.com>
2022-12-13 12:01:39 -0300

    falta poder eliminar ciertos gastos y darle estilo con CSS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputPart =  document.getElementById('inputPart')
 const btnPart =document.getElementById('btn-inputPart')
 const partContainer = document.getElementById('partContainer')
 const persBalanceContainer = document.getElementById('personal-balance-container')
+const servPayContainer = document.getElementById('severance-pay-container')
 const btnPurch = document.getElementById('btn-add-purchase')
 const pAP = document.getElementById('pAP') // este es el contenedor de los nombres que salen arriba para distribuir los gastos (arriba de los cuadros de colores)
 const pWPoptions = document.getElementById('pWPoptions')
@@ -20,10 +21,6 @@ window.addEventListener('keydown',(e)=>{  /*este evento keydown, se dispara cuan
 btnPart.addEventListener('click', addParticipant)// tmb disparo la funcion si tocan el boton para agregar
 
 btnPurch.addEventListener('click',addPurchase)
-
-
-
-
 
 
 ///////////// Aca voy creando todas las funciones que necesito/////////////////
@@ -56,16 +53,15 @@ function addParticipant(){
         newOptionpWP.innerText = `${inputPart.value}`
         pWPoptions.appendChild(newOptionpWP)
 
-        participants[inputPart.value] = {idNumber: p, name:inputPart.value, personalBalance:{"mustPay":0, "payedDone":0,"Balance":0}}   // agrego el particpante acual al objetro "participants"        
+        participants[inputPart.value] = {idNumber: p, name:inputPart.value, personalBalance:{"mustPay":0, "payedDone":0,"balance":0}}   // agrego el particpante acual al objetro "participants"        
         crearCasilleroBlanco(Object.keys(participants).length) // Object.keys(MiObjeto), nos devuelve un array con las keys de nuestro objeto.
+        addToExistingPurchases()
         p ++ //aumneto el valor de para que el prox participante tenga otro valor.
         inputPart.value = ''
     }else {
         console.log('no se puede cargar una string vacio.')
     }
 }
-
-
 //aca creo la funcion que me tiene q crear los casilleros en blanco en la primera linea para despues poder tickiarlos y assignar los gastos.
 
 function crearCasilleroBlanco(n){
@@ -79,6 +75,26 @@ function crearCasilleroBlanco(n){
 
 }
 
+function addToExistingPurchases(){
+    for(let i=0; i <= g;i++){
+        const purchase = document.getElementsByClassName(`purchase-g${i}`)[0]
+        if(purchase !== undefined){
+            const tickContainer = purchase.lastElementChild
+            const tick = document.createElement('div')
+            tick.id = `tick-p${p}-g${i}`
+            tick.classList.add(`tick-p${p}`,`particularTick-g${i}`,'square')
+            tick.innerHTML = `<i class="fa fa-check"></i>`
+            tick.addEventListener('click', () => { 
+                tick.classList.toggle("activate")
+                GeneratePersonalBalance()
+                severancePay()
+            })
+            purchase.lastElementChild.appendChild(tick)
+        }
+    }
+}
+    
+
 //aca creo la funcion para eliminar los particiapantes 
 function deleteParticipant(part){
     const partToDelete = part
@@ -90,6 +106,7 @@ function deleteParticipant(part){
     deleteParticipantPeopleAfectedByPurchases(partIdNumber) //borro la columna de casilleros del participante eliminado
     delete participants[part.innerText] //aca elimino el participante del objeto participants.
     GeneratePersonalBalance()
+    severancePay()
     
 }
 
@@ -97,7 +114,6 @@ function deleteParticipant(part){
 function getIdNumber(name){
     const idNumber = participants[name]["idNumber"]
     return idNumber
-
 }
 
 ///////////Aca van las funcion es de agregar Gastos////////////////
@@ -111,7 +127,7 @@ function addPurchase(){
     console.log(pwdIDNumber)
     const ticks = createTicksCliked()
     const div = document.createElement('div')
-    div.classList.add("purchase", `purchase-p${pwdIDNumber}`)
+    div.classList.add("purchase", `purchase-p${pwdIDNumber}`,`purchase-g${g}`)
     div.innerHTML = `
         
         <div class="purchase-concept" id="pc-g${g}">${concept}</div>
@@ -124,6 +140,7 @@ function addPurchase(){
     agregarToggle()// le agrego el toggle("activate") a todos los nuevos clicks
     setInputsToZero()
     GeneratePersonalBalance()
+    severancePay()
     g++
 }
 
@@ -155,6 +172,7 @@ function agregarToggle(){
     elements.forEach(element => element.addEventListener('click', () => {
         element.classList.toggle('activate')
         GeneratePersonalBalance() // lo pongo aca para que se actualice solo cuando voy tickeando/destickeando
+        severancePay() //segun los ticks tiene que actualizar quien le paga a quien.
     }))
 }
 function setInputsToZero(){
@@ -203,7 +221,8 @@ function upGradePersonalBalance(){
         const amountEach = amount/purchaseConsumer
         for(let i=0;i<ticks.length;i++){ //no puedo unir este for con el de arriba, pq necesito el valor definitivo del purchaseConsumer.
             if(ticks[i].classList.contains('activate')){
-                const partcipantIDNumber = +ticks[i].id[6] //ATENCIONN TENGO ESTO VA FUNCIONAR HASTA 9 PARTICIPANTES:.. TENGO QUE VER DE SACAR el numero si tengo mas de 9 participantes.. 
+                //const partcipantIDNumber = +ticks[i].id[6] //ATENCIONN TENGO ESTO VA FUNCIONAR HASTA 9 PARTICIPANTES:.. TENGO QUE VER DE SACAR el numero si tengo mas de 9 participantes.. 
+                const partcipantIDNumber = +ticks[i].id.split('-')[1].substring(1)//aca saco el numero de participante que es.
                 for(let j = 0; j<participantsKey.length;j++ ){
                     if( partcipantIDNumber === participants[participantsKey[j]]['idNumber'] )
                         participants[participantsKey[j]]['personalBalance']['mustPay']+= amountEach
@@ -226,7 +245,7 @@ function personalBalaceToZero(){
 function personalBalanceBalance(){ // calculado el "payedDone" y el "MustPay", calculo el balance personal. .
     const participantskeys = Object.keys(participants)
     for(let i = 0; i < participantskeys.length; i++){
-        participants[participantskeys[i]]['personalBalance']['Balance']  = participants[participantskeys[i]]['personalBalance']['payedDone'] - participants[participantskeys[i]]['personalBalance']['mustPay']
+        participants[participantskeys[i]]['personalBalance']['balance']  = participants[participantskeys[i]]['personalBalance']['payedDone'] - participants[participantskeys[i]]['personalBalance']['mustPay']
     }
 }
 
@@ -247,9 +266,9 @@ function GeneratePersonalBalance(){
     const participantskeys = Object.keys(participants)
     for(let i = 0; i < participantskeys.length; i++){
         const name= participants[participantskeys[i]]['name']  
-        const mustPay = Math.round(participants[participantskeys[i]]['personalBalance']['mustPay'],2)
-        const payDone = Math.round(participants[participantskeys[i]]['personalBalance']['payedDone'],2)
-        const balance =  Math.round(participants[participantskeys[i]]['personalBalance']['Balance'],2)
+        const mustPay = participants[participantskeys[i]]['personalBalance']['mustPay'].toFixed(2)
+        const payDone = participants[participantskeys[i]]['personalBalance']['payedDone'].toFixed(2)
+        const balance =  participants[participantskeys[i]]['personalBalance']['balance'].toFixed(2)
         const div = document.createElement('div')
         div.classList.add('personal-balance-participant')
         div.innerHTML = `
@@ -261,3 +280,121 @@ function GeneratePersonalBalance(){
         persBalanceContainer.appendChild(div)
     }
 }
+
+function severancePay(){
+    let contador = 0;
+    let debtors = []
+    let creditors = []
+    const tranfers = []
+    const participantskeys = Object.keys(participants)
+    for(let i = 0; i < participantskeys.length; i++){
+        if(participants[participantskeys[i]]['personalBalance']['balance'] > 0){
+            creditors.push([participants[participantskeys[i]]['name'], participants[participantskeys[i]]['personalBalance']['balance']])
+        } else if(participants[participantskeys[i]]['personalBalance']['balance'] < 0){
+            debtors.push([participants[participantskeys[i]]['name'], participants[participantskeys[i]]['personalBalance']['balance']])
+        } 
+    }
+
+    console.log('----inicio------')
+    //console.log(allBalances)
+    console.log(debtors)
+    console.log(higherDebtor(debtors))
+    console.log(creditors)
+    console.log(higherCreditor(creditors))
+    console.log('----fin------')
+
+    while((debtors.length > 0 && creditors.length > 0)){  //pongo los 2 pq quizas uno quede con algun valor de deuda o beneficio producto de los redondeos..
+        let debtor = higherDebtor(debtors)
+        let creditor = higherCreditor(creditors)
+    
+        if(Math.abs(debtor[1]) > creditor[1]){
+            console.log("entre en 1 ")
+            tranfers.push([debtor[0],creditor[0],creditor[1]])
+            indexDebtor = findIndex(debtors,debtor) //aca obtengo que indice ocupa este deudor en el array de deudores y le tengo que bajar la deuda pq ya se creo la transferencia
+            debtors[indexDebtor][1] += creditor[1]  //aca le sumo a la deuda (que es  un numero negativo) lo que transfirio para ir descontandola.. (negativo + positivo = reducir/eliminar la deuda)
+            creditors = deletePerson(creditors,creditor) //como cancelo la deuda lo borro de los deudores
+        }else if(Math.abs(debtor[1]) < creditor[1]){
+            console.log("entre en 2 ")
+            tranfers.push([debtor[0],creditor[0],Math.abs(debtor[1])]) //aca le transfiero todo la deuda del deudor al creditor
+            indexCreditor = findIndex(creditors,creditor)//tengo que reducir el credito, pq no lo cancele pero si lo reduje.
+            creditors[indexCreditor][1] += debtor[1] //la deuda debtor[1] tiene signo negativo, por lo que hay q sumarlo para reducirla-
+            debtors = deletePerson(debtors,debtor) // como se le cancelo el credito lo borro de los creditores
+            console.log('creditor: ',creditors[indexCreditor][1],'deuda',debtor[1])
+        } else { //esto deberia darse al ultimo y es cuando quedan los ultimos 2. un deudor que le debe solo a un creditor.. por lo que trasnfiero y los borro a ambos. 
+            console.log("entre en 3 ") 
+            tranfers.push([debtor[0],creditor[0],creditor[1]])
+            debtors = deletePerson(debtors,debtor)
+            creditors = deletePerson(creditors,creditor) 
+        }
+        
+    }
+    createTransfers(tranfers)
+
+}
+
+// creo una funcion que me tire el mayor beneficiario de una array como "creditor"
+function higherCreditor(array){
+    let mayor = array[0] //supongo que el primer elemento es el mayor ydespues si encuentro otro mayor lo reemplazo.
+    for(let i=1; i<array.length; i++){
+        if( array[i][1] > mayor[1]){
+            mayor = array[i]
+        }
+    }
+    return mayor
+}
+//creo una funcion que me busque el mayor deudor.. (el que tiene el balance mas negativo)
+function higherDebtor(array){
+    let menor = array[0] //supongo que el primer elemento es el mayor ydespues si encuentro otro mayor lo reemplazo.
+    for(let i=1; i<array.length; i++){
+        if( array[i][1] < menor[1]){
+            menor = array[i]
+        }
+    }
+    return menor
+}
+//creo una funcion que me borre el participante higherCreditor o higherDebtor del array "debtors" o "creditors"
+function deletePerson(generalArray,personArray){
+    const index = findIndex(generalArray,personArray)
+    const newArray = generalArray.filter((_, idx) => idx != index);
+    return newArray
+}
+//aca busco el indice que te gno q eliminar en la funcion deletePerson
+function findIndex(generalArray,personArray){
+    let idx = 0;
+    for(let i = 0; i < generalArray.length;i++){
+        if(personArray[0] === generalArray[i][0]){
+            return idx
+        }else {
+            idx++
+        }
+    }
+}
+
+// creo la funcion para crear las transferecias en el DOM
+function createTransfers(array){
+    servPayContainer.innerHTML = `
+        <div class="head">
+            <h2> Severance Pay </h2>
+        </div>
+        <div class="severance-pay-title">
+            <div>Debtor</div>
+            <div>Amount</div>
+            <div>Beneficiary</div>
+        </div>
+    `
+    for(let i = 0;i < array.length;i++){
+        const debtor = array[i][0]
+        const creditor = array[i][1]
+        const amount = array[i][2].toFixed(2)
+        div = document.createElement('div')
+        div.classList.add('severance-pay-title')
+        div.innerHTML = `
+            <div>${debtor}</div>
+            <div>${amount}</div>
+            <div>${creditor}</div>
+        `
+        servPayContainer.appendChild(div)
+    }
+
+}
+
